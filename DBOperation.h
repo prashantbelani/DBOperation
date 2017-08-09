85f07799ad6a9d96b865005b220ec086471e6c13
prashantbelani <prashant.belani@gmail.com>
2017-08-09 16:25:10 +0530

    Added DBOperation Files

---
+++ DBOperation.h
@@ -0,0 +1,16 @@
+ 
+#import <Foundation/Foundation.h>
+#import <sqlite3.h>
+
+@interface DBOperation : NSObject 
+{
+}
+
++(void)OpenDatabase:(NSString*)path;  //Open the Database
++(void)finalizeStatements;//Closing and do the final statement at application exits
++(void)checkCreateDB;
++(int) getLastInsertId;
++(BOOL) executeSQL:(NSString *)sqlTmp;
++(NSMutableArray*) selectData:(NSString *)sql;
+
+@end
